36c25a21a2a993e39d3840c8477d84b4a74ccdfb
enajeh <e.najeh@gmail.com>
2022-06-25 11:54:21 +0200

    getArticles from backend (BDD)

--- src/components/article/one-article/one-article.js
+++ src/components/article/one-article/one-article.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./one-article.css";
 import { Link } from "react-router-dom";
 
+const pathImages = window.location.origin + "/images";
+
 function OneArticle(props) {
   return (
     <div className="article">
       <div className="article-content">
         <img
           className="article-image"
-          src={props.article.image}
+          src={`${pathImages}${props.article.imageurl}`}
           alt={props.article.titre}
         />
         <span className="article-title"> {props.article.titre} </span>
