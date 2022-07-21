31f9fb95b1297ff71bc7b7e803b36a823098155f
enajeh <e.najeh@gmail.com>
2022-07-21 22:39:25 +0200

    Fix redirect when add compte

--- src/components/compte/sEnregistrer.js
+++ src/components/compte/sEnregistrer.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Redirect } from 'react-router-dom';
 import axios from "axios";
+
 import './sEnregistrer.css'
 
 function SEnregistrer() {
     const { handleSubmit, register, errors } = useForm();
+    const [isRedirect, setIsRedirect] = useState(false)
  
     const onSubmit = (values) => {
         axios.post('http://localhost:3001/comptes', values, {
@@ -15,114 +18,119 @@ function SEnregistrer() {
             "Vous etes entrain d'enregistrer un nouveau compte: ",
             res.data
             );
-            
+            setIsRedirect(true)
         })
         .catch((err) => {
             console.log(err);
+            setIsRedirect(false);
         });
     };
 
-    return (
-        <div id="back">
-        <div id="sEnregistrer" className="section layout_padding">
-            <div className="container-fluid">
-                <div>
-                <div className="heading">
-                    <h2>Enregistrerez-nous !</h2>
-                </div>
-                <div className="full margin_top_20">
-                    <form  onSubmit={handleSubmit(onSubmit)}>
-                        <div className="row">
-                            <div className="col-25">
-                            <label htmlFor="" className="label">
-                                Nom Complet:{" "}
-                            </label>
-                            </div>
-                            <div className="col-75">
-                            <input
-                                type="text"
-                                name="nomComplet"
-                                placeholder="nomComplet"
-                                ref={register({
-                                required: "Required",
-                                pattern: {
-                                    minLength: 2,
-                                    maxLength: 100,
-                                    message: "invalide nomComplet",
-                                },
-                                })}
-                            />
-                            <span className="error">
-                                {errors.nomComplet && errors.nomComplet.message}
-                            </span>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-25">
-                            <label htmlFor="" className="label">
-                                Email:{" "}
-                            </label>
-                            </div>
-                            <div className="col-75">
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="email"
-                                ref={register({
-                                required: "Required",
-                                pattern: {
-                                    minLength: 2,
-                                    maxLength: 100,
-                                    message: "invalide email",
-                                },
-                                })}
-                            />
-                            <span className="error">
-                                {errors.email && errors.email.message}
-                            </span>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-25">
+    if(isRedirect === true) {
+        return <Redirect to='/home'/> 
+    } else {
+        return (
+            <div id="back">
+            <div id="sEnregistrer" className="section layout_padding">
+                <div className="container-fluid">
+                    <div>
+                    <div className="heading">
+                        <h2>Enregistrerez-nous !</h2>
+                    </div>
+                    <div className="full margin_top_20">
+                        <form  onSubmit={handleSubmit(onSubmit)}>
+                            <div className="row">
+                                <div className="col-25">
                                 <label htmlFor="" className="label">
-                                Mot de passe:{" "}
+                                    Nom Complet:{" "}
                                 </label>
-                            </div>
-                            <div className="col-75">
+                                </div>
+                                <div className="col-75">
                                 <input
-                                type="password"
-                                name="pwd"
-                                placeholder="Mot de passe"
-                                ref={register({
+                                    type="text"
+                                    name="nomComplet"
+                                    placeholder="nomComplet"
+                                    ref={register({
                                     required: "Required",
                                     pattern: {
-                                    minLength: 2,
-                                    maxLength: 100,
-                                    message: "invalide pwd",
+                                        minLength: 2,
+                                        maxLength: 100,
+                                        message: "invalide nomComplet",
                                     },
-                                })}
+                                    })}
                                 />
                                 <span className="error">
-                                {errors.pwd && errors.pwd.message}
+                                    {errors.nomComplet && errors.nomComplet.message}
                                 </span>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-75">
-                                <button className="btn add-submit-btn col-75" type="submit">
-                                    Enregistrer
-                                </button>
+    
+                            <div className="row">
+                                <div className="col-25">
+                                <label htmlFor="" className="label">
+                                    Email:{" "}
+                                </label>
+                                </div>
+                                <div className="col-75">
+                                <input
+                                    type="text"
+                                    name="email"
+                                    placeholder="email"
+                                    ref={register({
+                                    required: "Required",
+                                    pattern: {
+                                        minLength: 2,
+                                        maxLength: 100,
+                                        message: "invalide email",
+                                    },
+                                    })}
+                                />
+                                <span className="error">
+                                    {errors.email && errors.email.message}
+                                </span>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                            <div className="row">
+                                <div className="col-25">
+                                    <label htmlFor="" className="label">
+                                    Mot de passe:{" "}
+                                    </label>
+                                </div>
+                                <div className="col-75">
+                                    <input
+                                    type="password"
+                                    name="pwd"
+                                    placeholder="Mot de passe"
+                                    ref={register({
+                                        required: "Required",
+                                        pattern: {
+                                        minLength: 2,
+                                        maxLength: 100,
+                                        message: "invalide pwd",
+                                        },
+                                    })}
+                                    />
+                                    <span className="error">
+                                    {errors.pwd && errors.pwd.message}
+                                    </span>
+                                </div>
+                            </div>
+    
+                            <div className="row">
+                                <div className="col-75">
+                                    <button className="btn add-submit-btn col-75" type="submit">
+                                        Enregistrer
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 </div>
             </div>
             </div>
-        </div>
-        </div>
-    );
+        );
+    }    
 }
 
 export default SEnregistrer;
--- src/components/connexion/seConnecter.js
+++ src/components/connexion/seConnecter.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Redirect } from 'react-router-dom';
 import axios from "axios";
+
 
 import "./seConnecter.css";
 
 function SeConnecter() {
   const { handleSubmit, register, errors } = useForm();
+  const [isRedirect, setIsRedirect] = useState(false)
 
   const onSubmit = (values) => {
     console.log("Values: ", values);
@@ -15,89 +18,95 @@ function SeConnecter() {
       .then((res) => {
         if(res.status === 200 && res.data.length > 0){
           console.log("Vous etes connecté !", res);
+          setIsRedirect(true);
         }else{
           console.log("Vous n'etes pas connecté !", res);
+          setIsRedirect(false);
         }
         
       })
       .catch((err) => {
         console.log(err);
+        setIsRedirect(false);
       });
   };
 
+  if(isRedirect === true) {
+    return <Redirect to='/home'/> 
+  } else {
+    return (
+      <div id="back">
+        <div id="seConnecter" className="section layout_padding">
+          <div className="container-fluid">
+            <div className="col-md-6">
+              <div className="heading">
+                <h2>Contactez-nous !</h2>
+              </div>
+              <div className="full margin_top_20">
+                <form  onSubmit={handleSubmit(onSubmit)}>
+                  <div className="row">
+                    <div className="col-25">
+                      <label htmlFor="" className="label">
+                        Email:{" "}
+                      </label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="email"
+                        ref={register({
+                          required: "Required",
+                          pattern: {
+                            minLength: 2,
+                            maxLength: 100,
+                            message: "invalide email",
+                          },
+                        })}
+                      />
+                      <span className="error">
+                        {errors.email && errors.email.message}
+                      </span>
+                    </div>
+                  </div>
 
-  return (
-    <div id="back">
-      <div id="seConnecter" className="section layout_padding">
-        <div className="container-fluid">
-          <div className="col-md-6">
-            <div className="heading">
-              <h2>Contactez-nous !</h2>
-            </div>
-            <div className="full margin_top_20">
-              <form  onSubmit={handleSubmit(onSubmit)}>
-                <div className="row">
-                  <div className="col-25">
-                    <label htmlFor="" className="label">
-                      Email:{" "}
-                    </label>
+                  <div className="row">
+                    <div className="col-25">
+                      <label htmlFor="" className="label">
+                        Mot de passe:{" "}
+                      </label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="password"
+                        name="pwd"
+                        placeholder="Mot de passe"
+                        ref={register({
+                          required: "Required",
+                          pattern: {
+                            minLength: 2,
+                            maxLength: 100,
+                            message: "invalide pwd",
+                          },
+                        })}
+                      />
+                      <span className="error">
+                        {errors.pwd && errors.pwd.message}
+                      </span>
+                    </div>
                   </div>
-                  <div className="col-75">
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="email"
-                      ref={register({
-                        required: "Required",
-                        pattern: {
-                          minLength: 2,
-                          maxLength: 100,
-                          message: "invalide email",
-                        },
-                      })}
-                    />
-                    <span className="error">
-                      {errors.email && errors.email.message}
-                    </span>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-25">
-                    <label htmlFor="" className="label">
-                      Mot de passe:{" "}
-                    </label>
-                  </div>
-                  <div className="col-75">
-                    <input
-                      type="password"
-                      name="pwd"
-                      placeholder="Mot de passe"
-                      ref={register({
-                        required: "Required",
-                        pattern: {
-                          minLength: 2,
-                          maxLength: 100,
-                          message: "invalide pwd",
-                        },
-                      })}
-                    />
-                    <span className="error">
-                      {errors.pwd && errors.pwd.message}
-                    </span>
-                  </div>
-                </div>
-                
-                <button className="btn add-submit-btn col-75" type="submit">
-                  Enregistrer
-                </button>
-              </form>
+                  
+                  <button className="btn add-submit-btn col-75" type="submit">
+                    Enregistrer
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SeConnecter;
